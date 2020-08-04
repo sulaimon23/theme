f8f1684a92ced564cd4aeda36c1ef42fd808d858
sulaimon23 <tsulaimon21@yahoo.com>
2020-08-04 07:28:32 +0100

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let theme = localStorage.getItem('theme');
+if(theme == null){
+ setTheme('light')
+}else{
+ setTheme(theme)
+}
+
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for(var i=0; themeDots.length > i; i++){
+ themeDots[i].addEventListener('click',function(){
+  let mode = this.dataset.mode
+  console.log(mode)
+  setTheme(mode);
+ })
+}
+
+function setTheme(mode){
+ if(mode == 'light'){
+  document.getElementById('theme-style').href = 'style.css'
+ }
+ if(mode == 'red'){
+  document.getElementById('theme-style').href = 'red.css'
+ }
+ if(mode == 'green'){
+  document.getElementById('theme-style').href = 'green.css'
+ }
+ if(mode == 'blue'){
+  document.getElementById('theme-style').href = 'blue.css'
+ }
+ localStorage.setItem('theme', mode)
+}
